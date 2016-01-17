dbcbe5d396c9fcc84fab537fa9cfe78ae564db3c
Jonathan Latties <jon.latties@gmail.com>
2016-01-17 04:00:44 -0200

    add packagens and dependencies

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('angularNodeStormpathApp', [
+  'angularNodeStormpathApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
